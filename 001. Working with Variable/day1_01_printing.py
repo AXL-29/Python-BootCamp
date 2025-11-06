cc0a61a9f3cf049aa9a545aa04aa4bcbd6396e6e
AXL-29 <gimpaojade4@gmail.com>
2025-11-07 03:28:58 +0800

    Add initial printing example with comments explaining print function usage

---
+++ 001. Working with Variable/day1_01_printing.py
@@ -0,0 +1,7 @@
+# print() function - show or display an output.
+# "" or '' - use to define the set of string not code.
+# SyntaxError: EOL while scanning string literal - means that the interpreter reached 
+#              the end of a line (EOL = End Of Line) while it was still expecting the end of a string 
+#              — in other words, a string wasn’t closed properly.
+
+print("Hello World!")
