1aeeeb5fd5e8d89ed6481c3e6c85f6fee8873180
AXL-29 <gimpaojade4@gmail.com>
2025-11-07 03:58:03 +0800

    Add string manipulation examples with comments

---
+++ 001. Working with Variable/day1_02_string_manipulation.py
@@ -0,0 +1,9 @@
+# \n = to add another line
+print("Hello World!\nHello World!")
+
+# + = use for string concatenation or combine 2 or more strings.
+print("Hello" + " Jade Mark")
+
+# IndentationError: unexpected indent - means that Python found a line of code 
+#                   that is indented more than it should be, or not aligned 
+#                   properly with the rest of the code.
