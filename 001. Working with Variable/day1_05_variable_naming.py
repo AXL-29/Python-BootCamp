216da026b2bd007024bef6a6abe07b0497403da9
AXL-29 <gimpaojade4@gmail.com>
2025-11-07 04:49:55 +0800

    Add variable naming rules and example usage

---
+++ 001. Working with Variable/day1_05_variable_naming.py
@@ -0,0 +1,12 @@
+# Variable Naming Rules:
+# 1. Make sure your variable names are descriptive
+# 2. Don't have spaces between words.
+# 3. Don't start with numbers.
+# 4. Don't use special words like print or input.
+
+name = input("What is your name: ")
+length = len(name)
+print(length)
+
+# NameError: a NameError happens when you try to use a name 
+# (usually a variable or function) that Python doesn’t know yet.
