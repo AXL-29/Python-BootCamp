a5a0444b729a3d527e7b95702622fca1629fa9af
AXL-29 <gimpaojade4@gmail.com>
2025-11-08 17:46:48 +0800

    Add basic mathematical operations examples and explanations

---
+++ 002. Understanding Data Types and How to Manipulate Strings/day2_03_mathematical_operation.py
@@ -0,0 +1,16 @@
+# Basic Mathematical Operatos: +, -, *, / and **
+# PEMDASLR - Parethheses, Exponents, Multiplication/Division, and Addition/Subtraction (Left - Right).
+
+print("My age: " + str(25))
+print(123 + 456)    # addition
+print(7 - 4)        # subtraction
+print(12 * 5)       # multiplication
+print(15 / 4)       # division
+print(15 // 4)      # floor division
+print(15 % 4)       # modulus
+print(3 ** 4)       # exponential
+
+print(3 * 3 + 3 / 3 - 3) # 7.0
+
+# Change this code and make an output of 3: print(3 * 3 + 3 / 3 - 3)
+print(3 * (3 + 3) / 3 - 3)
