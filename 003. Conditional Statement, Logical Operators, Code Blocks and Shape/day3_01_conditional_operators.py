dcc5b679a341d2759a71a274977d08a3d276d9fa
AXL-29 <gimpaojade4@gmail.com>
2025-11-09 14:53:32 +0800

    Add initial implementation of control flow with height-based rollercoaster access

---
+++ 003. Conditional Statement, Logical Operators, Code Blocks and Shape/day3_01_conditional_operators.py
@@ -0,0 +1,29 @@
+# Control Flow - decides which part of your code runs based on conditions (like "if this happens, do that").
+# Python uses keywords like if, elif, and else for decision-making.
+
+# Syntax of if-else:
+# if condition:
+#     do this
+# else:
+#     do this
+
+# Indentation - means adding spaces before a line of code to show that it belongs to a group or block of code.
+
+print("Welcome to the rollercoaster!")
+height = float(input("What is your height in cm?: "))
+
+if height > 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you can't ride the rollercoaster.")
+
+# IndentationError: expected an indented block
+# This means Python expected some code to be indented, but it wasn't.
+
+# Comparison Operators:
+# >     Greater than
+# <     Less than
+# >=    Greater than or equal to
+# <=    Less than or equal to
+# ==    Equal to
+# !=    Not equal to
