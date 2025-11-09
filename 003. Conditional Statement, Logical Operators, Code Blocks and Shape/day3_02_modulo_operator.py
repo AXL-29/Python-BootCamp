c646a38d49b1cc7afe01503b85acc27837237326
AXL-29 <gimpaojade4@gmail.com>
2025-11-09 15:01:14 +0800

    Add implementation of modulo operator with odd/even check

---
+++ 003. Conditional Statement, Logical Operators, Code Blocks and Shape/day3_02_modulo_operator.py
@@ -0,0 +1,10 @@
+# Modulo Operator (%) - gives the remainder of a division.
+
+# Code Challenge: Check Odd or Even.
+
+number = int(input("Please input a number: "))
+
+if number % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
