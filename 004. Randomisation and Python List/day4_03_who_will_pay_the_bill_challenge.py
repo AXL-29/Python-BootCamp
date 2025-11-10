a13b9d269b2fa9b2c204736151f43e5ea4720ae6
AXL-29 <gimpaojade4@gmail.com>
2025-11-10 13:00:23 +0800

    Add Banker Roulette program to randomly select a bill payer from a list of friends

---
+++ 004. Randomisation and Python List/day4_03_who_will_pay_the_bill_challenge.py
@@ -0,0 +1,10 @@
+import random
+
+# Banker Roulette
+print("welcome to Banker Rolette!")
+
+friends = ["Jade", "Shane", "Joshua", "Johnrey", "Marlou", "Justin"]
+
+random_name = random.choice(friends)
+
+print(f"The one who will pay the bill is: {random_name}")
