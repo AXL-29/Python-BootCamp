c28f6705acae44b57c3f48dd3c7357d1061779bf
AXL-29 <gimpaojade4@gmail.com>
2025-11-11 05:43:42 +0800

    Add initial for loop example demonstrating iteration over a list of fruits

---
+++ 005. Python Loops/day5_01_for_loop.py
@@ -0,0 +1,14 @@
+# for loop - is used to repeat a blocked of code multiple times, usually to go through (or iterate over)
+# a sequence like a list, string, tuple, dictionary, or range of numbers.
+
+# syntax:
+# for variable in sequence:
+#   do something.
+
+# variable - takes each value from the sequence one by one.
+# sequence - can be list, string, range, etc.
+
+fruits = ["apple", "mango", "pear", "orange", "banana"]
+
+for fruit in fruits:
+    print(fruit)
