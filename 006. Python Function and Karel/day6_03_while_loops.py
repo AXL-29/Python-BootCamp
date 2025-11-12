901d8b894f955b1285920cb7f6c16b83f8e82206
AXL-29 <gimpaojade4@gmail.com>
2025-11-12 12:46:22 +0800

    Add while loop example with explanation and functionality

---
+++ 006. Python Function and Karel/day6_03_while_loops.py
@@ -0,0 +1,16 @@
+# while loop - is used repeat code of block as long as a certain condition is True.
+# The loop just keeps running until the condition becomes False.
+
+# Syntax:
+# while condition_is_true:
+#   code to run repeatedly.
+
+# condition - a statement that is checked before each loop.
+# if it's True, the code inside the loops runs.
+# if it's False, the loop stops.
+
+number = 10
+
+while number > 0:
+    number -= 1
+    print(number)
