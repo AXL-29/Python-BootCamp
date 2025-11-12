5668dbc0fa08f079ad34dbf716fd7870e2699b4d
AXL-29 <gimpaojade4@gmail.com>
2025-11-13 05:08:33 +0800

    Updated Hangman display logic to use a placeholder string for underscores and correctly reveal guessed letters in their positions while maintaining spacing.

--- 007. Hangman Game/hangman.py
+++ 007. Hangman Game/hangman.py
@@ -4,20 +4,20 @@ word_list = ["aardvark", "baboon", "camel"]
 
 # TODO-1:   Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
 chosen_word = random.choice(word_list)
-print(chosen_word)
+placeholder = ""
+for char in chosen_word:
+    placeholder += "_ "
+
+print(placeholder)
 
 # TODO-2:   Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess = input("Guess a letter: ").lower()
 
-display = []
+display = " a"
 # TODO-3: Check if the letter the user (guess) is one to letters in the chosen_word. Print "right" if it is, "wrong" if it's not.
 for letter in chosen_word:
     if letter == guess:
-        print("right")
-        display.append(guess)
+        display += letter
     else:
-        print("wrong")
-        display.append("_")
-display = " ".join(display)
+        display += " _ "
 print(display)
-
