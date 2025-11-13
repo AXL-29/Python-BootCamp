863c0ef6f49d83aa48526fd6750160705126effa
AXL-29 <gimpaojade4@gmail.com>
2025-11-13 16:23:45 +0800

    Fix display reset issue in Hangman by using a list to preserve guessed letters and prevent infinite loop.

--- 007. Hangman Game/hangman.py
+++ 007. Hangman Game/hangman.py
@@ -2,22 +2,21 @@ import random
 
 word_list = ["aardvark", "baboon", "camel"]
 
-# TODO-1:   Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
 chosen_word = random.choice(word_list)
-placeholder = ""
-for char in chosen_word:
-    placeholder += "_ "
+print(chosen_word)  # for testing
 
-print(placeholder)
+# Create placeholders for each letter
+display = ["_"] * len(chosen_word)
+print("".join(display))
 
-# TODO-2:   Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter: ").lower()
+while "_" in display:
+    guess = input("Guess a letter: ").lower()
 
-display = " a"
-# TODO-3: Check if the letter the user (guess) is one to letters in the chosen_word. Print "right" if it is, "wrong" if it's not.
-for letter in chosen_word:
-    if letter == guess:
-        display += letter
-    else:
-        display += " _ "
-print(display)
+    # Update display if the guess is correct
+    for index, letter in enumerate(chosen_word):
+        if letter == guess:
+            display[index] = letter
+
+    print("".join(display))
+
+print("You guessed the word!")
