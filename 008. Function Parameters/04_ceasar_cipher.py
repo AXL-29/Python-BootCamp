3db8d029c5dd68db60ce7ea8b42374c0a714325e
AXL-29 <gimpaojade4@gmail.com>
2025-11-16 05:22:59 +0800

    Add a simple implementation of Caesar Cipher

---
+++ 008. Function Parameters/04_ceasar_cipher.py
@@ -0,0 +1,15 @@
+alphabet = [
+    "a", "b", "c", "d", "e", "f", "g",
+    "h", "i", "j", "k", "l", "m", "n",
+    "o", "p", "q", "r", "s", "t", "u",
+    "v", "w", "x", "y", "z"
+]
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
+text = input("Type your message: ").lower()
+shift = int(input("Type the shift number: "))
+
+encrypted_text = []
+
+def encrypt(original_text, shift_amount):
+    print("test")
