6b4a8f2c3ea7de31add6d8effe6d66205b00e0be
AXL-29 <gimpaojade4@gmail.com>
2025-11-17 05:06:13 +0800

    Convert student exam scores into descriptive grades by iterating over `student_scores`, filling a `student_grades` dictionary, and printing the final results.

---
+++ 009. Dictionaries, Nesting and the Secret Auction/02_grading_program.py
@@ -0,0 +1,28 @@
+# Don't change this dictionary
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+
+# Don't modify the code above 👆
+
+# Create the empty dictionary
+student_grades = {}
+
+# Convert scores to grades
+for student in student_scores:
+    score = student_scores[student]
+    
+    if score >= 91 and score <= 100:
+        student_grades[student] = "Outstanding"
+    elif score >= 81 and score <= 90:
+        student_grades[student] = "Exceeds Expectations"
+    elif score >= 71 and score <= 80:
+        student_grades[student] = "Acceptable"
+    else:
+        student_grades[student] = "Fail"
+
+print(student_grades)
