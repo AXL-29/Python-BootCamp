ce3b9a7a0457ee5f0764c75f4b98500b26a5944b
AXL-29 <gimpaojade4@gmail.com>
2025-11-18 17:20:01 +0800

    Add blackjack game, implement initial code like variables and user input with conditional statement.

---
+++ 011. Blackjack Capstone Project/blackjack.py
@@ -0,0 +1,15 @@
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+# TODO-1: Create a user input that ask "Do you want to play a game of Blackjack? Type 'y' or 'n'"
+play = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
+
+# TODO-2: Conditional statement if 'y' continue, else and the game.
+player_first_card = 0
+player_second_card = 0
+player_third_card = 0
+
+if play == 'y':
+    # TODO-3: Display your cards, and computer's first card
+    
+else:
+    # end of game
