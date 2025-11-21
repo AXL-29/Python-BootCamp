f7c6c0a7877761de1e69d35ae528d4993458928a
AXL-29 <gimpaojade4@gmail.com>
2025-11-21 16:56:22 +0800

    Learn about global constat and demonstrate few examples

---
+++ 012. Scope and Number Guessing Project/04_global_constant_and_global_scope.py
@@ -0,0 +1,12 @@
+# In Python, a global constant is a variable defined at the top level of your file (global scope)
+# that you don't intend to change.
+
+# Python doesn't have true constant built into the language, so we follow a naming convention to say "please treat this as a constant".
+
+# How we usually define a global constant:
+#     - Written in UPPER_SNAKE_CASE
+#     - Defined outside any function or class
+
+PI = 3.14159        # global constant
+MAX_USERS = 100     # global constant
+API_URL = "https://example.com/api"
