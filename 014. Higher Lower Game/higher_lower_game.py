1a45c38f0fabc18ddb77205475ec8107e03e4376
AXL-29 <gimpaojade4@gmail.com>
2025-11-24 16:04:49 +0800

    Refactor the logo and fix the bug in random picking of account in game_data

--- 014. Higher Lower Game/higher_lower_game.py
+++ 014. Higher Lower Game/higher_lower_game.py
@@ -2,8 +2,18 @@ import art
 from game_data import data
 import random
 
+
 print(art.logo)
 
-for _ in range(len(data)):
-    compare_a = random.choice(data)
-    print(compare_a)
+random_account_a = random.choice(data)
+print(f"Compare A: {random_account_a['name']}, {random_account_a['description']}, {random_account_a['country']}.")
+
+print(art.vs)
+
+random_account_b = random.choice(data)
+print(f"Compare B: {random_account_b['name']}, {random_account_b['description']}, {random_account_b['country']}.")
+
+user_choice = input("Who has more followers? Type 'A' or 'B': ").upper()
+
+if user_choice == 'A':
+
