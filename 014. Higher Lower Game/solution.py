a6da9ef1558d5a90f0348504db3244b1cde5761e
AXL-29 <gimpaojade4@gmail.com>
2025-11-26 15:48:09 +0800

    Create a check_answer() function to determine whether the user’s input is correct. If the user_guess is correct, return True and increase the score. Then repeat the game as long as the answer is not False (wrong).

--- 014. Higher Lower Game/solution.py
+++ 014. Higher Lower Game/solution.py
@@ -10,49 +10,58 @@ def format_data(account):
     account_country = account["country"]
     return f"{account_name}, a {account_description}, from{account_country}"
 
+# 5. Check if user is correct
 def check_answer(user_guess, a_followers, b_followers):
-    """Take a user's guess and the follower counts and returns if they got it right."""
-    a_followers = 0
-    b_followers = 0
+    """Return True if the user's guess is correct, False if wrong."""
 
+    # 5.2 Use if statement to check if user is correct.
+    if a_followers > b_followers:
+        return user_guess == "a"
+    else:
+        return user_guess == "b"
+
+# 1. Display Art
+print(logo)
+score = 0
+is_game_over = False
+
+# 8. Make the game repeatable.
+while not is_game_over:
+    # 2. Generate a random account from the game data.
+    account_a = random.choice(data)
+    account_b = random.choice(data)
+
+    # 2.1 Re-generate account_b if equal to account_a
+    if account_b == account_a:
+        account_b = random.choice(data)
+
+    # 3.1 Print the account data
+    print(f"Compare A: {format_data(account_a)}")
+    print(vs)
+    print(f"Against B: {format_data(account_b)}")
+
+    # 4. Ask user for a guess, make sure `guess` is guaranteed to be either "a" or "b"
     while True:
-        if user_guess == "a":
-            a_followers = account_a["follower_count"]
-        elif user_guess == "b":
-            b_followers = account_b["follower_count"]
+        guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+        if guess in ["a", "b"]:
+            break
         else:
             print("Invalid input! Please choose between 'A' and 'B' only. Thank you!")
 
 
-
-# 1. Display Art
-print(logo)
-
-# 2. Generate a random account from the game data.
-account_a = random.choice(data)
-account_b = random.choice(data)
-
-# 2.1 Re-generate account_b if equal to account_a
-if account_b == account_a:
-    account_b = random.choice(data)
-
-# 3.1 Print the account data
-print(f"Compare A: {format_data(account_a)}")
-print(vs)
-print(f"Against B: {format_data(account_b)}")
-
-# 4. Ask user for a guess.
-guess = input("Who has more followers? Type 'A' or 'B': ").lower()
-
-# 5. Check if user is correct
     # 5.1 Get the follower count of each account
-follower_count_a = account_a["follower_count"]
-follower_count_b = account_a["follower_count"]
+    follower_count_a = account_a["follower_count"]
+    follower_count_b = account_b["follower_count"]
 
-check_answer(guess, follower_count_a, follower_count_b)
+    is_correct = check_answer(guess, follower_count_a, follower_count_b)
 
-    # 5.2 Use if statement to check if user is correct.
-# 6. Give user feedback
-# 7. Score Keeping
-# 8. Make the game repeatable.
+    # 6. Give user feedback on their guess
+    # 7. Score Keeping
+    if is_correct:
+        score += 1
+        print(f"You're right! Current score: {score}")
+    else:
+        print(f"Sorry, that's wrong! Final score: {score}")
+        is_game_over = True
 # 9. Making account at position B become the next account at position A.
