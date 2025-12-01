b62d249027ea6a0f4c93b7d04643d45c28ba2641
AXL-29 <gimpaojade4@gmail.com>
2025-12-01 10:52:27 +0800

    **Commit message:**
    `Implement basic coffee machine logic with resource check and report feature`

--- 016. Local Development Environment Setup and the Coffee Machine/main.py
+++ 016. Local Development Environment Setup and the Coffee Machine/main.py
@@ -7,13 +7,20 @@
 
 from coffee_data import resources, menu
 
+
 def check_resources(coffee_resources, coffee_menu):
-    for key, value in coffee_menu.items():
-        if key > coffee_resources.key():
-            print("invalid")
+    for ingredient, required_amount in coffee_menu.items():
+        available_amount = coffee_resources.get(ingredient, 0)
+
+        if available_amount < required_amount:
+            print(f"Sorry, not enough {ingredient}.")
+            return False
+    print("We have enough resources!")
+    return True
 
 
 user_prompt = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
 # TODO: 1. Print report of all coffee machine resources.
 if user_prompt == "report":
     print("Coffee current resources")
@@ -23,8 +30,18 @@ if user_prompt == "report":
         elif key == "coffee":
             unit = "g"
         else:
-            unit = ""   # no unit if your don't need one.
+            unit = ""
         print(f"{key.title()}: {value}{unit}")
 
 # TODO: 2. Check resources sufficient to make a coffee order.
+elif user_prompt in menu:
+    # get the ingredients needed for that drink
+    drink = menu[user_prompt]
+    ingredients_needed = drink["ingredients"]
 
+    # call your check_resources function
+    if check_resources(resources, ingredients_needed):
+        print(f"Making your {user_prompt} ☕...")
+        # later: you'll subtract the ingredients from resources here
+else:
+    print("Invalid choice.")
