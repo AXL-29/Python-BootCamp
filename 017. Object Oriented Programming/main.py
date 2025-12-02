1bca3287aa7514559c063d7ef7dc259fb2c3035d
AXL-29 <gimpaojade4@gmail.com>
2025-12-03 07:14:38 +0800

    Learn OOP about attributes, methods, object and class.

---
+++ 017. Object Oriented Programming/main.py
@@ -0,0 +1,20 @@
+# Procedural Programming - is a programming paradigm (style of coding) that is based on procedures, 
+# also called functions, methods, or routines. It focuses on writing step-by-step instructions that 
+# tell the computer what to do in a logical sequence.
+
+# Object Oriented Programming - is a style of programming where you structure your code using objects.
+# An object represents a real-world thing and contains both data (variables) and behaviors (functions/methods).
+
+# attributes - stores data about each object.
+# methods - this performs an action using the object's attributes.
+# object - is an actual thing made from class, each object has its own value.
+# class - is like a blueprint, it defines what an object will have (attributes) and can do (methods)
+
+from turtle import Turtle, Screen
+
+# object = class()
+johny = Turtle()
+print(johny)
+
+my_screen = Screen()
+print(my_screen.canvheight)
