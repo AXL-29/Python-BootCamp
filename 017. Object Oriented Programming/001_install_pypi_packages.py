f32c80d7705ca688dc62b9a0d268af1a8cd046a9
AXL-29 <gimpaojade4@gmail.com>
2025-12-04 03:16:41 +0800

    Add PrettyTable object creation and column formatting example

--- 017. Object Oriented Programming/001_install_pypi_packages.py
+++ 017. Object Oriented Programming/001_install_pypi_packages.py
@@ -4,5 +4,13 @@
 
 # Example: pip install PrettyTable
 
-import prettytable
+from prettytable import PrettyTable
 
+table = PrettyTable()       # Creating a new object.
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])     # Calling a method associated with the object.
+table.add_column("Type", ["Electric", "Water", "Fire"])                     # Calling a method associated with the object.
+
+# Changing an Object Attributes:
+table.align = "l"
+
+print(table)
