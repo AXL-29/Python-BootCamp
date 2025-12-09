172a7e45e6765d13457ef940422785c78c5caee2
AXL-29 <gimpaojade4@gmail.com>
2025-12-09 11:00:37 +0800

    Learn about creating a class, and how to use it as an object.

---
+++ 018. Benefits of OOP/creating_a_class.py
@@ -0,0 +1,21 @@
+# a class - is a blueprint for creating objects. It defines the data (attributes)
+# and the behaviors (methods) that the objects created from it will have.
+
+# Class = blueprint / template
+# Object (instance) = actual item created from that blueprint.
+
+# Why do we use classes?
+# Classes let you:
+    # Group related data and functions together
+    # Organize code better
+    # Reuse code
+    # Represent real-world things in programming (like a car, student, bank account, etc.)
+
+# Basic Structure of a Class:
+
+# class naming convention use the PascalCase (ex: UserCountSize)
+
+class User:
+    pass            # pass - continue to the next line of the code
+
+user_1 = User()
