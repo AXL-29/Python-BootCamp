eec07a2ddf01444407d0c683986de23fdaf4bbca
JadeMark <gimpaojade4@gmail.com>
2025-12-11 22:39:43 +0800

    Import a turtle module, and use the Turtle, Screen objects and it's methods

---
+++ 019. Turtle Graphics, Tuples and Importing Modules/001_understanding_turtle_graphics_and_how_to_use_the_documentation.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+jadey_the_turtle = Turtle()
+jadey_the_turtle.shape("turtle")
+jadey_the_turtle.color("red")
+
+jadey_the_turtle.forward(100)
+jadey_the_turtle.right(90)
+
+screen = Screen()
+screen.exitonclick()
