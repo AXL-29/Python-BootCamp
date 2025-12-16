8deeed36ffe296fc25b82e945b75fe4d322a09ef
Jade <gimpaojade4@gmail.com>
2025-12-17 03:17:18 +0800

    Add Ball class implementation to handle ball movement in Pong game

--- 022. Build Pong Game/main.py
+++ 022. Build Pong Game/main.py
@@ -1,6 +1,7 @@
 import time
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
 
 screen = Screen()
 
@@ -11,13 +12,17 @@ screen.tracer(0)
 
 right_paddle = Paddle(x_cor=350, y_cor=0)
 left_paddle = Paddle(x_cor=-350, y_cor=0)
+ball = Ball()
 
 screen.listen()
 screen.onkey(right_paddle.go_up, "Up")
 screen.onkey(right_paddle.go_down, "Down")
+screen.onkey(left_paddle.go_up, "w")
+screen.onkey(left_paddle.go_down, "s")
 
 while True:
     screen.update()
     time.sleep(0.1)
+    ball.move()
 
 screen.exitonclick()
