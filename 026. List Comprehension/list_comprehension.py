6791f0d1348bd2c5946f438becdc4c541fc839b7
Jade <gimpaojade4@gmail.com>
2025-12-23 04:26:30 +0800

    Add examples of conditional list comprehension and filtering

--- 026. List Comprehension/list_comprehension.py
+++ 026. List Comprehension/list_comprehension.py
@@ -12,3 +12,16 @@ print(letter_list)
 
 doubled = [d * 2 for d in range(1, 5)]
 print(doubled)
+
+# conditional list comprehension - lets you add conditions to decide which items are included
+# and/or how they're transformed.
+
+# conditioanl filtering - keeps items only if the condition is true. 
+# syntax: [expression for item in iterable if condition]
+even_numbers = [n for n in numbers if n % 2 == 0]
+print(even_numbers)
+
+# conditional expression (if-else) - this changes the value added to the list, not whether it's included.
+# syntax: [true_value if condition else false_value for item in iterable]
+labels = ["Even" if n % 2 == 0 else "Odd" for n in numbers]
+print(labels)
