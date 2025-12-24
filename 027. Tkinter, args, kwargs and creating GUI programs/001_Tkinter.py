a45119d8d0c3f0dd694b5e2e7e078b6dbb7da8d6
Jade <gimpaojade4@gmail.com>
2025-12-25 04:35:46 +0800

    Learn about Tkinter GUI

---
+++ 027. Tkinter, args, kwargs and creating GUI programs/001_Tkinter.py
@@ -0,0 +1,25 @@
+# Tkinter is a built-in GUI (Graphical User Interface) library.
+#   - It lets you create windows, buttons, text boxes, labels, forms and more
+#   - So users can click instead of typing in the terminal.
+
+# Common Tkinter Components(Widgets)
+# Widget	Purpose
+# Tk()	    Creates the main window
+# Label	    Displays text
+# Button	Clickable button
+# Entry	    Input box
+# Text	    Multi-line text
+# Canvas	Draw shapes/images
+
+import tkinter
+
+window = tkinter.Tk()
+window.title("My GUI Program")
+window.minsize(width=800, height=800)
+
+# Label
+label = tkinter.Label(text="I am a Label", font=("Courier", 24, "bold"))
+label.pack()
+
+
+window.mainloop()
