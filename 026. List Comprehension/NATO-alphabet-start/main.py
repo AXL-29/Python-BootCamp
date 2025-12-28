69c0cc57fd5ab366a199ed251949ec7df4c83169
Jade <gimpaojade4@gmail.com>
2025-12-29 05:13:28 +0800

    Add exception handling for user input.

--- 026. List Comprehension/NATO-alphabet-start/main.py
+++ 026. List Comprehension/NATO-alphabet-start/main.py
@@ -1,12 +1,16 @@
 import pandas
 
-
 #TODO 1. Create a dictionary in this format:
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter your name: ").upper()
-phonetic_code = [nato_dict[letter] for letter in user_input if letter in nato_dict]
-print(phonetic_code)
-
+while True:
+    try:
+        user_input = input("Enter your name: ").upper()
+        phonetic_code = [nato_dict[letter] for letter in user_input]
+        print(phonetic_code)
+        break
+    
+    except KeyError:
+        print("Sorry, only letter in the alphabet please.")
