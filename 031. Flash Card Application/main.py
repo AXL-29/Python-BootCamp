6749303e180f744854e79abc26aff8cde0e6b9a0
Jade <gimpaojade4@gmail.com>
2025-12-30 04:28:46 +0800

    feat: add flash_card function to display random French word.

--- 031. Flash Card Application/main.py
+++ 031. Flash Card Application/main.py
@@ -1,14 +1,29 @@
 from tkinter import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+def flash_card():
+    data = pandas.read_csv("./data/french_words.csv")
+    word_dict = {row.French:row.English for (index, row) in data.iterrows()}
+
+    french_word, english_word = random.choice(list(word_dict.items()))
+
+    canvas.itemconfig(title_text, text="French")
+    canvas.itemconfig(word_text, text=french_word)
+
 # UI Setup:
-windows = Tk()
+window = Tk()
 
-windows.title("Flash Card Application")
-windows.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
+window.title("Flash Card Application")
+window.config(bg=BACKGROUND_COLOR, 
+              padx=50, pady=50)
 
-canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
+canvas = Canvas(width=800, 
+                height=526, 
+                highlightthickness=0, 
+                bg=BACKGROUND_COLOR)
 front_img = PhotoImage(file="./images/card_front.png")
 canvas.create_image(400, 263, image=front_img)
 
@@ -27,15 +42,23 @@ wrong_button = PhotoImage(file="./images/wrong.png")
 right_button = PhotoImage(file="./images/right.png")
 
 wrong = Button(
+        command=flash_card,
         image=wrong_button,
         highlightthickness=0,
-        borderwidth=0
-        ).grid(row=1, column=0, padx=50, pady=50)
+        borderwidth=0,
+        takefocus=False,
+        activebackground=BACKGROUND_COLOR
+        ).grid(row=1, column=0, padx=50)
 
 right = Button(
+        command=flash_card,
         image=right_button,
         highlightthickness=0,
-        borderwidth=0
-        ).grid(row=1, column=1, padx=50, pady=50)
+        borderwidth=0,
+        takefocus=False,
+        activebackground=BACKGROUND_COLOR
+        ).grid(row=1, column=1, padx=50)
 
-windows.mainloop()
+
+
+window.mainloop()
