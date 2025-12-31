5a6a712e00cbffb09beff448ff8f7904bafdb6e5
Jade Mark Gimpao <gimpaojade4@gmail.com>
2025-12-31 14:38:23 +0800

    Initial implementation of Automated Birthday Wisher.

--- 032. Send Email & Manage Dates/Birthday Wisher/main.py
+++ 032. Send Email & Manage Dates/Birthday Wisher/main.py
@@ -1,17 +1,13 @@
-import smtplib
+##################### Extra Hard Starting Project ######################
 
-EMAIL = "gimpaojade4@gmail.com"
-APP_PASSWORD = "lhlx yniz kiix ohhr"
+# 1. Update the birthdays.csv
 
-with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-    connection.starttls()
-    connection.login(EMAIL, APP_PASSWORD)
-    connection.sendmail(
-        from_addr=EMAIL,
-        to_addrs="gimpao22@gmail.com",
-        msg="""Subject: Test
+# 2. Check if today matches a birthday in the birthdays.csv
 
-            Hello from Python!
-            """
-    )
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+# 4. Send the letter generated in step 3 to that person's email address.
+
+
+
 
