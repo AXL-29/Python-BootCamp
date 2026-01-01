92f447d1f4bbf312d458b370d15e35aaafd2b06a
Jade Mark Gimpao <gimpaojade4@gmail.com>
2026-01-01 12:31:36 +0800

    Generate personalized birthday letters from CSV data

--- 032. Send Email & Manage Dates/Birthday Wisher/main.py
+++ 032. Send Email & Manage Dates/Birthday Wisher/main.py
@@ -1,13 +1,24 @@
-##################### Extra Hard Starting Project ######################
+from datetime import datetime
+import random
+import pandas
 
-# 1. Update the birthdays.csv
+today = datetime.now()
+today_month = today.month
+today_day = today.day
 
-# 2. Check if today matches a birthday in the birthdays.csv
+data = pandas.read_csv("birthdays.csv").to_dict(orient="records")
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+for person in data:
+    if person["month"] == today_month and person["day"] == today_day:
 
-# 4. Send the letter generated in step 3 to that person's email address.
+        letter_file = random.choice([
+            "letter_1.txt",
+            "letter_2.txt",
+            "letter_3.txt"
+        ])
 
+        with open(f"letter_templates/{letter_file}") as file:
+            letter = file.read()
+            personalized_letter = letter.replace("[NAME]", person["name"])
 
-
-
+        print(personalized_letter)  # test output first
