1c342299d2967f122602dcc8b68ffd12412c62bf
Jade <gimpaojade4@gmail.com>
2026-01-02 03:42:55 +0800

    Learn about Application Programming Interfaces (APIs)

---
+++ 033. API Endpoints & API Parameters - ISS Overhead Notifier/001_APIs.py
@@ -0,0 +1,14 @@
+# An Application Programming Interface (API) is a set of commands, functions, protocols, and objects.
+# that programmers can use to create software or interact with an external system.
+
+# An API endpoint is a specific URL when you request data from an API.
+
+# API Parameters let you customize what data the API returns.
+    # They are usually added to the URL after a "?", and separated by "&".
+    # Example: https://api.sunrise-sunset.org/json?lat=14.5995&lng=120.9842&formatted=0
+
+    # lat - Latitude
+    # lng - Longitude
+    # formatted=0 - returns time in 24-hrs (ISO) format
+
+
