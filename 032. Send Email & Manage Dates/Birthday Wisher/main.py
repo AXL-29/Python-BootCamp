1addbc0f1b94b22128ee765ec4aa1357b7bbf130
Jade Mark Gimpao <gimpaojade4@gmail.com>
2026-01-01 13:21:17 +0800

    Fix email template indentation for clean birthday emails

--- 032. Send Email & Manage Dates/Birthday Wisher/main.py
+++ 032. Send Email & Manage Dates/Birthday Wisher/main.py
@@ -1,24 +1,40 @@
-from datetime import datetime
 import random
 import pandas
+import smtplib
+from datetime import datetime
+
+APP_PASSWORD = "password"
+EMAIL = "gimpaojade4@gmail.com"
+
+RANDOM_LETTER = random.choice([
+    "letter_1.txt",
+    "letter_2.txt",
+    "letter_3.txt"
+])
 
 today = datetime.now()
-today_month = today.month
-today_day = today.day
+today_tuple = (today.month, today.day)
 
-data = pandas.read_csv("birthdays.csv").to_dict(orient="records")
+data = pandas.read_csv("birthdays.csv")
+birthday_dict = {
+    (row.month, row.day): row
+    for (_, row) in data.iterrows()
+}
 
-for person in data:
-    if person["month"] == today_month and person["day"] == today_day:
+if today_tuple in birthday_dict:
+    birthday_person = birthday_dict[today_tuple]
+    name = birthday_person["name"]
+    recipient_email = birthday_person["email"]
 
-        letter_file = random.choice([
-            "letter_1.txt",
-            "letter_2.txt",
-            "letter_3.txt"
-        ])
+    with open(f"letter_templates/{RANDOM_LETTER}") as file:
+        content = file.read()
+        personalized_letter = content.replace("[NAME]", name).strip()
 
-        with open(f"letter_templates/{letter_file}") as file:
-            letter = file.read()
-            personalized_letter = letter.replace("[NAME]", person["name"])
-
-        print(personalized_letter)  # test output first
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(EMAIL, APP_PASSWORD)
+        connection.sendmail(
+            from_addr=EMAIL,
+            to_addrs=recipient_email,
+            msg=f"Subject: Happy Birthday\n\n{personalized_letter}"
+        )
