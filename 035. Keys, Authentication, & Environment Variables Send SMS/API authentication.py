25fd39d4358586f3e870c32ca409ffcc0db8f4ea
Jade <gimpaojade4@gmail.com>
2026-01-08 04:45:33 +0800

    Initial commit and description of API Key and API authentication

---
+++ 035. Keys, Authentication, & Environment Variables Send SMS/API authentication.py
@@ -0,0 +1,17 @@
+# API authentication is the process of proving who you are when your 
+# program communicates with another system through an API
+
+# Without Authentication:
+    # Anyone could access the API 
+    # Data could be stolen or abused
+    # Services could be misused or overcharged
+
+# Why API authentication is important:
+    # Security - protects data
+    # Usage tracking - rate limits & billing
+    # Access control - who can do what
+    # Audit logs - trace actions
+
+# API Key is a unique secret string that identifies whos is making an API request.
+# API key = ID card for your application.
+
