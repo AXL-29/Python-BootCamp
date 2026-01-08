f552d8e97db97133ecd7bf732262c412f0ca62bc
Jade Mark Gimpao <gimpaojade4@gmail.com>
2026-01-08 19:54:45 +0800

    Fix weather data parsing by correctly indexing forecast list

--- 035. Keys, Authentication, & Environment Variables Send SMS/Weather Notification/main.py
+++ 035. Keys, Authentication, & Environment Variables Send SMS/Weather Notification/main.py
@@ -1,12 +1,22 @@
 import requests
 
+OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
 API_KEY = "48289d3f3be5b573baf1f5c508beeca7"
 
 parameter = {
-    "q": "London,UK",
+    "lat": 14.477140,
+    "lon": 120.892242,
     "appid": API_KEY,
+    "cnt": 4,
 }
 
-response = requests.get(url="https://api.openweathermap.org/data/2.5/weather", params=parameter)
-print(response.json())
+response = requests.get(url=OWM_ENDPOINT, params=parameter)
+response_code = response.status_code
+data = response.json()
 
+for forecast in data.get("list", []):
+    if forecast.get("weather"):
+        weather_id =  forecast["weather"][0]["id"]
+
+        if weather_id < 700:
+            print("Bring an umbrella")
