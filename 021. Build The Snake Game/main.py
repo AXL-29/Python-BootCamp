647b0b3cc3305afd86e3cd36d2b765f8b4b9b56a
Jade Mark Gimpao <gimpaojade4@gmail.com>
2026-01-12 17:36:25 +0800

    Create snake body and snake movement

---
+++ 021. Build The Snake Game/main.py
@@ -0,0 +1,25 @@
+import time
+from turtle import Screen
+from snake import Snake
+
+screen = Screen()
+
+screen.setup(height=600, width=600)
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+screen.exitonclick()
