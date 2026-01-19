8c7d1388020935c4d9281d063eb9e7af7b101018
Jade <gimpaojade4@gmail.com>
2026-01-20 03:50:35 +0800

    feat: fetch top 3 Tesla news articles when stock price changes significantly

--- 036. Stock Trading News Alert/main.py
+++ 036. Stock Trading News Alert/main.py
@@ -10,6 +10,7 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = "EC5XGS83V10USF9G"
+NEWS_API_KEY = "0afbbc1cb1124276b67efaddae81bd4f"
 
 # STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -20,11 +21,11 @@ stock_params = {
     "apikey": STOCK_API_KEY
 }
 
-response = requests.get(STOCK_ENDPOINT, params=stock_params, verify=False)
-response.raise_for_status()
+stock_response = requests.get(STOCK_ENDPOINT, params=stock_params, verify=False)
+stock_response.raise_for_status()
 
-data = response.json()
-time_series = data["Time Series (Daily)"]
+stock_data = stock_response.json()
+time_series = stock_data["Time Series (Daily)"]
 
 daily_prices = [value for (key, value) in time_series.items()]
 yesterday_close = float(daily_prices[0]["4. close"])
@@ -35,18 +36,20 @@ diff_percent = round((difference / float(yesterday_close)) * 100, 2)
 print(yesterday_close, day_before_close)
 print(difference)
 print(diff_percent)
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
 
+# STEP 2: https://newsapi.org/ 
+# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
 if diff_percent > 0.20:
-    print("Get News")
+    news_params = {
+        "qInTitle": COMPANY_NAME,
+        "apiKey": NEWS_API_KEY
+    }
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params, verify=False)
+    news_response.raise_for_status
 
-    ## STEP 2: https://newsapi.org/ 
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
-
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
-
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
-
+    news_article = news_response.json()["articles"]
+    three_articles = news_article[:3]
+    print(three_articles)
 
     ## STEP 3: Use twilio.com/docs/sms/quickstart/python
     #to send a separate message with each article's title and description to your phone number. 
